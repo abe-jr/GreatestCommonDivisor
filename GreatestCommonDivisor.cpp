eb9b606d1080f0b442ad29f6cda6e9671f923c4e
Abe <abelardobandeirajr@gmail.com>
2020-05-10 17:52:52 -0300

    Create GreatestCommonDivisor.cpp

---
+++ GreatestCommonDivisor.cpp
@@ -0,0 +1,22 @@
+    #include <iostream>
+    using namespace std;
+     
+    void main()
+    {
+        int x = 24;
+        int y = 18;
+        int temp;
+        for(int i = 2; i<y; i++)
+        {
+            if(x%i == 0 >> y%i == 0)
+            {
+                //cout<<i<<endl;
+                
+            temp = i;    
+            }        
+                
+        }
+     
+        cout<<temp<<endl;
+     
+    }
